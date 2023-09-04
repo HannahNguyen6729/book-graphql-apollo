bc56161877c290c55185da22441f86e83d555b4f
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-09-04 15:52:34 +0300

    connect to db

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,37 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs } from './schema/schema.js';
 import { resolvers } from './resolver/resolver.js';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+// Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
+mongoose.set('strictQuery', false);
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log('mongoDB connected');
+  } catch (error) {
+    console.log(error);
+  }
+};
+connectDb();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
+await server.start();
 
-console.log(`🚀  Server ready at: ${url}`);
+app.use('/graphql', cors(), json(), expressMiddleware(server));
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
